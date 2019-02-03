4868234473967adecfacf4753901c0747ecfd3f4
Dennis Orbison <orbison.dennis@gmail.com>
2019-02-03 00:24:42 -0800

    configuring knex and dbConfig

--- knexfile.js
+++ knexfile.js
@@ -5,40 +5,14 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './bookr.sqlite3'
+      filename: '.data/bookr.sqlite3'
     }
   },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+  useNullAsDefault: true,
+  migrations: {
+    directory: './data/migrations'
   },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+  seeds: {
+    directory: './data/seeds'
   }
-
-};
+}
