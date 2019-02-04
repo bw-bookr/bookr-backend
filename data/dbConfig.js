a87106fba3130d2fc919c53cbf5b144c6ddd9fc0
Dennis Orbison <orbison.dennis@gmail.com>
2019-02-04 15:05:32 -0800

    updated dbconfig

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,5 +1,11 @@
-const knex = require('knex');
-const knexConfig = require('../knexfile.js');
-const db = knex(knexConfig.development);
+// const knex = require('knex');
+// const knexConfig = require('../knexfile.js');
+// const db = knex(knexConfig.development);
 
-module.exports = db; 
+// module.exports = db; 
+
+// setup for postgress
+const dbEngine = process.env.DB || 'development';
+const config = require('../knexfile.js')[dbEngine];
+
+module.exports = require('knex')(config);
